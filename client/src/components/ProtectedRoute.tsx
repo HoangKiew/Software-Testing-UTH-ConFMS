914edfca6a165bc7264060814fc4c97d129510af
Hoang Kiew <hoangmaikieu1@gmail.com>
2025-12-31 16:51:56 +0700

    feat(client): add ProtectedRoute component for authentication

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,20 +1,17 @@
-// import { Navigate, useLocation } from 'react-router-dom';
-// import { tokenUtils } from '../utils/token';
+import { Navigate, useLocation } from 'react-router-dom';
+import { tokenUtils } from '../utils/token';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  // TEMPORARY: Disable authentication for UI development
-  // TODO: Re-enable when backend is ready and authentication is needed
+  const location = useLocation();
+  const isAuthenticated = tokenUtils.hasToken();
 
-  // const location = useLocation();
-  // const isAuthenticated = tokenUtils.hasToken();
-
-  // if (!isAuthenticated) {
-  //   return <Navigate to="/login" state={{ from: location }} replace />;
-  // }
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
   return <>{children}</>;
 };
